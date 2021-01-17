dcf3250498ffbe59869a7e4df40d35d55b1f2d7a
Oskar <saturdaysheroes.business@gmail.com>
2021-01-17 12:46:47 +0100

    attempt automatic login

--- src/epic.ts
+++ src/epic.ts
@@ -5,28 +5,38 @@ import {default as ClientLoginAdapter} from "epicgames-client-login-adapter";
 let accounts = getAccounts();
 let clients = [];
 
-function initClients(): Promise<Array<EpicAccount>> {
+function initClients(): Promise<Array<Launcher>> {
     return new Promise(async (resolve, reject) => {
         for (let i in accounts) {
             let login = accounts[i];
             let client = new Launcher();
+            let options = {
+                email: login.email,
+                password: login.password
+            }
             
             if (!await client.init()) {
                 reject("Error occurred while initializing launcher.");
                 return;
             };
-
-            let auth = await ClientLoginAdapter.init({email: login.email, password: login.password});
-            let code = await auth.getExchangeCode();
             
-            await auth.close();
+            try {
+                await client.login(options);
+                clients.push(client);
+            } catch(ex) {
+                console.log("[LOGIN] Automatic login failed.");
+                let auth = await ClientLoginAdapter.init(options);
+                let code = await auth.getExchangeCode();
+            
+                await auth.close();
 
-            if (!await client.login(null, code)) {
-                reject("Error occurred while logging in.");
-                return;
+                if (!await client.login(null, code)) {
+                    reject("Error occurred while logging in.");
+                    return;
+                };
+
+                clients.push(client);
             };
-
-            clients.push(client);
         };
 
         resolve(clients);
