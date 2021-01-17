776a3c250268dccfb6478869c4b4093dc6fdad97
Oskar <saturdaysheroes.business@gmail.com>
2021-01-17 11:54:33 +0100

    init clients on startup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("lol");
+import * as Clients from "./epic";
+
+// Initialize all clients. 
+Clients.init()
+    .then(clients => {
+        console.log(clients);
+    })
+    .catch(err => {
+        console.log(err);
+    });
